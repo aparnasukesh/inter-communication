7c035e42c09ac94b036fec966cbb944482b092e7
aparnasukesh <aparnasukesh0723@gmail.com>
2024-09-09 18:33:34 +0530

    reset send email

--- notification/notification_grpc.pb.go
+++ notification/notification_grpc.pb.go
@@ -19,7 +19,8 @@ import (
 const _ = grpc.SupportPackageIsVersion8
 
 const (
-	EmailService_SendEmail_FullMethodName = "/notification.EmailService/SendEmail"
+	EmailService_SendEmail_FullMethodName              = "/notification.EmailService/SendEmail"
+	EmailService_SendResetPassWordEmail_FullMethodName = "/notification.EmailService/SendResetPassWordEmail"
 )
 
 // EmailServiceClient is the client API for EmailService service.
@@ -27,6 +28,7 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type EmailServiceClient interface {
 	SendEmail(ctx context.Context, in *EmailRequest, opts ...grpc.CallOption) (*EmailResponse, error)
+	SendResetPassWordEmail(ctx context.Context, in *EmailRequest, opts ...grpc.CallOption) (*EmailResponse, error)
 }
 
 type emailServiceClient struct {
@@ -47,11 +49,22 @@ func (c *emailServiceClient) SendEmail(ctx context.Context, in *EmailRequest, op
 	return out, nil
 }
 
+func (c *emailServiceClient) SendResetPassWordEmail(ctx context.Context, in *EmailRequest, opts ...grpc.CallOption) (*EmailResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(EmailResponse)
+	err := c.cc.Invoke(ctx, EmailService_SendResetPassWordEmail_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // EmailServiceServer is the server API for EmailService service.
 // All implementations must embed UnimplementedEmailServiceServer
 // for forward compatibility
 type EmailServiceServer interface {
 	SendEmail(context.Context, *EmailRequest) (*EmailResponse, error)
+	SendResetPassWordEmail(context.Context, *EmailRequest) (*EmailResponse, error)
 	mustEmbedUnimplementedEmailServiceServer()
 }
 
@@ -61,6 +74,9 @@ type UnimplementedEmailServiceServer struct {
 
 func (UnimplementedEmailServiceServer) SendEmail(context.Context, *EmailRequest) (*EmailResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SendEmail not implemented")
+}
+func (UnimplementedEmailServiceServer) SendResetPassWordEmail(context.Context, *EmailRequest) (*EmailResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SendResetPassWordEmail not implemented")
 }
 func (UnimplementedEmailServiceServer) mustEmbedUnimplementedEmailServiceServer() {}
 
@@ -93,6 +109,24 @@ func _EmailService_SendEmail_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
+func _EmailService_SendResetPassWordEmail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(EmailRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(EmailServiceServer).SendResetPassWordEmail(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: EmailService_SendResetPassWordEmail_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(EmailServiceServer).SendResetPassWordEmail(ctx, req.(*EmailRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // EmailService_ServiceDesc is the grpc.ServiceDesc for EmailService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -103,6 +137,10 @@ var EmailService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "SendEmail",
 			Handler:    _EmailService_SendEmail_Handler,
+		},
+		{
+			MethodName: "SendResetPassWordEmail",
+			Handler:    _EmailService_SendResetPassWordEmail_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
